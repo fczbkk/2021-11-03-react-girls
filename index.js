87c5a2643836b8d8e0d194022dc7e5aa8ea87f0b
fczbkk <riki@fczbkk.com>
2021-11-03 20:12:15 +0100

    smart hints

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ function getRandomNumber () {
 window.addEventListener('load', function () {
 
   let randomNumber = getRandomNumber()
+  console.log(randomNumber)
+
+  let highestGuessedNumber = 100
+  let lowestGuessedNumber = 1
 
   let submitButton = document.querySelector('.submitButton')
   let guessInput = document.querySelector('.userInput')
   let messageElement = document.querySelector('.message')
   let highGuessHint = document.querySelector('.highGuess')
   let lowGuessHint = document.querySelector('.lowGuess')
+  let winNumberHint = document.querySelector('.winNumber')
+  let startAgainButton = document.querySelector('.startAgainButton')
+
+  startAgainButton.addEventListener('click', function () {
+    // document.location = document.location
+    randomNumber = getRandomNumber()
+    console.log(randomNumber)
+
+    messageElement.innerHTML = 'GUESS!'
+    winNumberHint.innerHTML = '?'
+    highGuessHint.innerHTML = '100'
+    lowGuessHint.innerHTML = '1'
+  })
 
   submitButton.addEventListener('click', function () {
     let guessedNumber = Number(guessInput.value)
@@ -19,17 +36,25 @@ window.addEventListener('load', function () {
       messageElement.innerHTML = 'Niekde je chyba.'
     } else if (guessedNumber === randomNumber) {
       messageElement.innerHTML = 'Gratulujem, uhadla si!'
+      winNumberHint.innerHTML = String(guessedNumber)
     } else if (guessedNumber > randomNumber) {
       messageElement.innerHTML = 'Myslim si mensi cislo.'
-      highGuessHint.innerHTML = String(guessedNumber)
+      if (guessedNumber < highestGuessedNumber) {
+        highGuessHint.innerHTML = String(guessedNumber)
+        highestGuessedNumber = guessedNumber
+      }
     } else {
       messageElement.innerHTML = 'Myslim si vetsi cislo.'
-      lowGuessHint.innerHTML = String(guessedNumber)
+      if (guessedNumber > lowestGuessedNumber) {
+        lowGuessHint.innerHTML = String(guessedNumber)
+        lowestGuessedNumber = guessedNumber
+      }
     }
 
-  })
+    guessInput.value = ''
+    guessInput.focus()
 
-  console.log('myslim si cislo', randomNumber)
+  })
 
 })
 
