3a4515bab52b242e0a1b1f6d230652babfa24f36
fczbkk <riki@fczbkk.com>
2021-11-03 20:36:54 +0100

    toggle visibility of guess panel

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ window.addEventListener('load', function () {
   let highestGuessedNumber = 100
   let lowestGuessedNumber = 1
 
+  let counter = 0
+
   let submitButton = document.querySelector('.submitButton')
   let guessInput = document.querySelector('.userInput')
   let messageElement = document.querySelector('.message')
@@ -17,9 +19,13 @@ window.addEventListener('load', function () {
   let lowGuessHint = document.querySelector('.lowGuess')
   let winNumberHint = document.querySelector('.winNumber')
   let startAgainButton = document.querySelector('.startAgainButton')
+  let guessPanel = document.querySelector('.guessPanel')
 
   startAgainButton.addEventListener('click', function () {
     // document.location = document.location
+
+    counter = 0
+
     randomNumber = getRandomNumber()
     console.log(randomNumber)
 
@@ -27,26 +33,31 @@ window.addEventListener('load', function () {
     winNumberHint.innerHTML = '?'
     highGuessHint.innerHTML = '100'
     lowGuessHint.innerHTML = '1'
+
+    guessPanel.style.visibility = 'visible'
   })
 
   submitButton.addEventListener('click', function () {
+    counter = counter + 1
+
     let guessedNumber = Number(guessInput.value)
 
     if (isNaN(guessedNumber)) {
       messageElement.innerHTML = 'Niekde je chyba.'
     } else if (guessedNumber === randomNumber) {
-      messageElement.innerHTML = 'Gratulujem, uhadla si!'
+      messageElement.innerHTML = `Gratulujem, uhadla si na ${counter} pokusov!`
       winNumberHint.innerHTML = String(guessedNumber)
+      guessPanel.style.visibility = 'hidden'
     } else if (guessedNumber > randomNumber) {
       messageElement.innerHTML = 'Myslim si mensi cislo.'
       if (guessedNumber < highestGuessedNumber) {
-        highGuessHint.innerHTML = String(guessedNumber)
+        highGuessHint.innerHTML = String(guessedNumber - 1)
         highestGuessedNumber = guessedNumber
       }
     } else {
       messageElement.innerHTML = 'Myslim si vetsi cislo.'
       if (guessedNumber > lowestGuessedNumber) {
-        lowGuessHint.innerHTML = String(guessedNumber)
+        lowGuessHint.innerHTML = String(guessedNumber + 1)
         lowestGuessedNumber = guessedNumber
       }
     }
