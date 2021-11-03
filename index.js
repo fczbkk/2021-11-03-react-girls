285600676f65d8d2027a546510b9e478655d8903
fczbkk <riki@fczbkk.com>
2021-11-03 19:01:39 +0100

    basic functionality

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// TODO
+window.addEventListener('load', function () {
+
+  let randomNumber = getRandomNumber()
+
+  let submitButton = document.querySelector('.submitButton')
+  let guessInput = document.querySelector('.userInput')
+
+  submitButton.addEventListener('click', function () {
+    let guessedNumber = Number(guessInput.value)
+
+    if (guessedNumber === randomNumber) {
+      console.log('Gratulujem, uhadla si!')
+    } else {
+      console.log('Nespravny tip.')
+    }
+
+  })
+
+  console.log('myslim si cislo', randomNumber)
+
+  function getRandomNumber () {
+    return Math.floor(Math.random() * 100) + 1
+  }
+
+})
+
+
