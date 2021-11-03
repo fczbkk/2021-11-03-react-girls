d9c2e1da57fc9c228914471906cb5c80bff81212
fczbkk <riki@fczbkk.com>
2021-11-03 19:51:33 +0100

    basic functionality of game

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
+function getRandomNumber () {
+  return Math.floor(Math.random() * 100) + 1
+}
+
 window.addEventListener('load', function () {
 
   let randomNumber = getRandomNumber()
 
   let submitButton = document.querySelector('.submitButton')
   let guessInput = document.querySelector('.userInput')
+  let messageElement = document.querySelector('.message')
+  let highGuessHint = document.querySelector('.highGuess')
+  let lowGuessHint = document.querySelector('.lowGuess')
 
   submitButton.addEventListener('click', function () {
     let guessedNumber = Number(guessInput.value)
 
-    if (guessedNumber === randomNumber) {
-      console.log('Gratulujem, uhadla si!')
+    if (isNaN(guessedNumber)) {
+      messageElement.innerHTML = 'Niekde je chyba.'
+    } else if (guessedNumber === randomNumber) {
+      messageElement.innerHTML = 'Gratulujem, uhadla si!'
+    } else if (guessedNumber > randomNumber) {
+      messageElement.innerHTML = 'Myslim si mensi cislo.'
+      highGuessHint.innerHTML = String(guessedNumber)
     } else {
-      console.log('Nespravny tip.')
+      messageElement.innerHTML = 'Myslim si vetsi cislo.'
+      lowGuessHint.innerHTML = String(guessedNumber)
     }
 
   })
 
   console.log('myslim si cislo', randomNumber)
-
-  function getRandomNumber () {
-    return Math.floor(Math.random() * 100) + 1
-  }
 
 })
 
